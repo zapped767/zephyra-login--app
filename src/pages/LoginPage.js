cfec90e42ff7dc69efe883d7654b83e967b24ef7
zapped767 <pathmanadhanarigaran16@gmail.com>
2025-06-01 16:36:22 +0530

    create page component add 2 pages

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+function LoginPage() {
+  return (
+    <div className="login-page">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ src/pages/SignupPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignupForm from '../components/SignupForm';
+
+function SignupPage() {
+  return (
+    <div className="signup-page">
+      <SignupForm />
+    </div>
+  );
+}
+
+export default SignupPage;
